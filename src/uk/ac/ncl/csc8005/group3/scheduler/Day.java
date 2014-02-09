36ff17b2e97d9644fe1b36055af9b7b91259914b
Luke McMahon <luke91uk@gmail.com>
2014-02-09 16:37:28 +0000

    More stuff added. hopefuly should have scheduler class constructor correct.

---
+++ src/uk/ac/ncl/csc8005/group3/scheduler/Day.java
@@ -0,0 +1,5 @@
+package uk.ac.ncl.csc8005.group3.scheduler;
+
+public class Day {
+
+}
--- src/uk/ac/ncl/csc8005/group3/scheduler/Scheduler.java
+++ src/uk/ac/ncl/csc8005/group3/scheduler/Scheduler.java
@@ -2,6 +2,7 @@ package uk.ac.ncl.csc8005.group3.scheduler;
 
 import java.util.*;
 import java.io.*;
+import java.text.*;
 
 
 public class Scheduler {
@@ -15,13 +16,18 @@ public class Scheduler {
 	private File file;
 	private DatabaseIO db;
 	
-	public Scheduler(String file, Date examStart, Date examEnd){
+	final long millisecondsInADay = 1000 * 60 * 60 * 24;
+	
+	public Scheduler(String file, String examStart, String examEnd){
 		this.file= new File(file);
 		db = new DatabaseIO(this.file);
 		modules=new ArrayList<Module>(db.getModule());
 	    rooms= new ArrayList<Room>(db.getRooms());
-	    days=new ArrayList<Day>();
-	    numberOfDays=examStart-examEnd;
+	    days=new ArrayList<Day>();	
+	    this.examStart=new SimpleDateFormat("d MMMM, yyyy", Locale.ENGLISH).parse(examStart);
+	    this.examEnd=new SimpleDateFormat("d MMMM, yyyy", Locale.ENGLISH).parse(examEnd);;
+	    
+	    numberOfDays = (int) ((this.examStart.getTime() - this.examEnd.getTime())/ millisecondsInADay );
 	}
 	
 	public Schedule generateSchedule(){
