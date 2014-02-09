1b0f76c7120f8724a0fda2a173c5d8d570189583
Luke McMahon <luke91uk@gmail.com>
2014-02-09 15:46:11 +0000

    Begin writing scheduler class... needs to many other classes first.

--- src/uk/ac/ncl/csc8005/group3/scheduler/Scheduler.java
+++ src/uk/ac/ncl/csc8005/group3/scheduler/Scheduler.java
@@ -1,5 +1,33 @@
 package uk.ac.ncl.csc8005.group3.scheduler;
 
-public class Scheduler {
+import java.util.*;
+import java.io.*;
 
+
+public class Scheduler {
+	private ArrayList<Module> modules;
+	private ArrayList<Room> rooms;
+	private ArrayList<Day> days;
+	private Date examStart;
+	private Date examEnd;
+	private int numberOfDays;
+	
+	private File file;
+	private DatabaseIO db;
+	
+	public Scheduler(String file, Date examStart, Date examEnd){
+		this.file= new File(file);
+		db = new DatabaseIO(this.file);
+		modules=new ArrayList<Module>(db.getModule());
+	    rooms= new ArrayList<Room>(db.getRooms());
+	    days=new ArrayList<Day>();
+	    numberOfDays=examStart-examEnd;
+	}
+	
+	public Schedule generateSchedule(){
+		
+		
+	}
+	
+	
 }
