a665b1a0a7429257bc0ae2cbda9d12bc0764aa84
Jessica <jessking2@gmail.com>
2014-02-13 16:21:29 +0000

    added some basic db connect code

---
+++ src/uk/ac/ncl/csc8005/group3/scheduler/Connect.java
@@ -0,0 +1,39 @@
+package uk.ac.ncl.csc8005.group3.scheduler;
+import java.sql.*;
+
+public class Connect {
+
+	   public static void main (String[] args)
+	   {
+	       Connection conn = null;
+
+	       try
+	       {
+	           String userName = "t8005t2";
+	           String password = ".oweRaps";
+	           String url = "jdbc:mysql://server.ncl.ac.uk/database";
+	           Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+	           conn = DriverManager.getConnection (url, userName, password);
+	           System.out.println ("Database connection established");
+	       }
+	       catch (Exception e)
+	       {
+	           System.err.println ("Cannot connect to database server");
+	           System.err.println (e.getMessage ());
+	       }
+	       finally
+	       {
+	           if (conn != null)
+	           {
+	               try
+	               {
+	                   conn.close ();
+	                   System.out.println ("Database connection terminated");
+	               }
+	               catch (Exception e) { /* ignore close errors */ }
+	           }
+	       }
+	   }	
+	
+	
+}
