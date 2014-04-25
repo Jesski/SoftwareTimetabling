d172f6fa13d751e6b82f3e71943bc45f1680b98d
Luke McMahon <luke91uk@gmail.com>
2014-04-25 15:33:54 +0100

    stuff

--- src/uk/ac/ncl/csc8005/group3/scheduler/Day.java
+++ src/uk/ac/ncl/csc8005/group3/scheduler/Day.java
@@ -7,12 +7,22 @@ public class Day {
 	private HashSet<Module> scheduledModules;
 	private int dayNumber;
 
+	/**
+	 * Constructor for objects of type day
+	 * @param dayNumber number of day in the schedule
+	 * @param rooms all room avilable to hold exams
+	 */
 	public Day(int dayNumber, ArrayList<Room> rooms) {
 		this.rooms = new ArrayList<Room>(rooms);
 		scheduledModules = new HashSet<Module>();
 		this.dayNumber=dayNumber;
 	}
 
+	/**
+	 * Attempts to add a module to the day, if module cannot be added returns false.
+	 * @param module the module to be added
+	 * @return false if module was not added successfully, true otherwise.
+	 */
 	public boolean addModule(Module module){
 		boolean attemptToSchedule=true;
 		boolean coupled = false;
@@ -20,7 +30,7 @@ public class Day {
 
 		if (scheduledModules.size() != 0){
 			for (Module moduleTwo : scheduledModules) {
-				if (checkCoupledModules(module, moduleTwo) == -1) {
+				if (checkCoupledModules(module, moduleTwo) != 0) {
 					//ie cannot be scheduled today, as already scheduled one of its clashed modules
 					//can be changed to allow for coupled modules to be scheduled on same day.. will need re-write of room/times class
 					return false;
@@ -51,18 +61,26 @@ public class Day {
 		return false; //should never reach here.
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "Day [rooms=" + rooms + "]";
 	}
 
 	//check this!!!!
+	/**
+	 * Returns number of pupils in one module that are also in another module.
+	 * @param moduleOne first module to be checked
+	 * @param moduleTwo second moduel to be checked
+	 * @return value of coipling, zero if modules are not coupled.
+	 */
 	public int checkCoupledModules(Module moduleOne, Module moduleTwo) {
-		boolean coupled = false;
 		int coupledValue=0;
 		
 		if(moduleOne.getCoupledModules()==null){ // ie if module 1 has no coupled modules, then cannot be coupled so return zero.
-			return -1;
+			return 0;
 		}
 		
 		Map<String, Integer> moduleOneCoupledModules = moduleOne.getCoupledModules(); // what if coupled modules ==null? Throws exception!!
@@ -72,17 +90,17 @@ public class Day {
 			Map.Entry<String, Integer> temp = (Map.Entry<String, Integer>) it.next();
 
 			if (temp.getKey().equals(moduleTwo.getId())) {
-				coupledValue=coupledValue+temp.getValue();
-				coupled=true;
+				coupledValue=coupledValue+temp.getValue();	
 			}
 		}
 		
-		if(coupled==false){
-			return -1; // if not coupled return -1
-		}
-		return coupledValue; // return number of coupled modules (ie number of students also taking other modules already scheduled on same day).
+		return coupledValue; // return value of coupled modules (ie number of students also taking other modules already scheduled on same day).
 	}
 
+	/**
+	 * @param module
+	 * @return
+	 */
 	public boolean removeModule(Module module) {
 		for (Room lookForRoom : rooms) {
 			if (lookForRoom.removeModule(module) == true) 
@@ -92,6 +110,10 @@ public class Day {
 	}
 	
 	///  is this needed?
+	/**
+	 * @param module
+	 * @return
+	 */
 	public boolean lookFor(Module module) {
 		for (Module lookingforModule : scheduledModules) {
 			if (module.getId().equals(lookingforModule.getId())) {
--- src/uk/ac/ncl/csc8005/group3/scheduler/Test/DayTest.java
+++ src/uk/ac/ncl/csc8005/group3/scheduler/Test/DayTest.java
@@ -7,7 +7,9 @@ import java.util.HashMap;
 
 import org.junit.Test;
 
+import uk.ac.ncl.csc8005.group3.scheduler.Day;
 import uk.ac.ncl.csc8005.group3.scheduler.Module;
+import uk.ac.ncl.csc8005.group3.scheduler.Room;
 
 public class DayTest {
 
@@ -16,25 +18,41 @@ public class DayTest {
 		fail("Not yet implemented");
 	}
 
+	
+	@Test 
+	public void testCheckCoupledModulesNoModule(){
+		fail("Not yet implemented");
+	}
+	
 	@Test
 	public void testCheckCoupledModules() {
+		//create two test modules which are coupled.
 		ArrayList<String> clashedModuels1 = new ArrayList<String>();
 		clashedModuels1.add("CSC8001");
 		clashedModuels1.add("CSC8002");
+		
 		HashMap<String, Integer> coupledModules1 = new HashMap<String, Integer>();
 		coupledModules1.put("CSC8004", 10);
 
-		Module testModule1 = new Module("CSC8004", clashedModuels1,
+		Module testModule1 = new Module("CSC8003", clashedModuels1,
 				coupledModules1, 1.00, 15, "CMP");
 		
 		
 		ArrayList<String> clashedModuels2 = new ArrayList<String>();
 		clashedModuels1.add("CSC8001");
 		clashedModuels1.add("CSC8002");
+		
 		HashMap<String, Integer> coupledModules2 = new HashMap<String, Integer>();
-		coupledModules1.put("CSC8004", 10);
+		coupledModules2.put("CSC8003", 10);
 
-		Module testModule2 = new Module("CSC8004", clashedModuels2,	coupledModules2, 1.00, 15, "CMP");
+		Module testModule2 = new Module("CSC8004", clashedModuels2,	coupledModules2, 1.00, 10, "CMP");
+		
+		//create test room
+		ArrayList<Room> Rooms= new ArrayList<Room>();
+		Rooms.add(new Room("LT1", "CMP", 9.00, 17.00, 100));
+		Day testDay = new Day(1, Rooms);
+		
+		System.out.println(testDay.checkCoupledModules(testModule1, testModule2));
 		
 	}
 
